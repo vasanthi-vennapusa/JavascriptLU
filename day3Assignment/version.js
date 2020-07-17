1f75ed108fe993143038b3d8edb111691e255598
vasanthi-vennapusa <66934745+vasanthi-vennapusa@users.noreply.github.com>
2020-07-17 13:50:04 +0530

    first commit

---
+++ day3Assignment/version.js
@@ -0,0 +1,10 @@
+let char1=prompt("getting os:",'android');//getting user input
+let char2=Number(prompt("getting version:",'9'));
+//hi=char1.slice(0);
+//ui=char1.slice(1);
+console.log(`the operating system ${char1} and version is 
+ an      ${char2}`);
+ let char=['android','php'];
+ console.log(`the operating system ${char.slice(0)}and version is ${char.slice(1)}`);
+
+                                        
